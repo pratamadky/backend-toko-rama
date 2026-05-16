a5c6a2289f96e76188c434ab4f74580060f527ff
pratamadky <dkyskyy87@gmail.com>
2026-05-16 14:13:52 +0700

    fix: use Railway MySQL env variables

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
+# database.py
 import mysql.connector
 import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get("MYSQLHOST"),
+        host=os.environ.get("MYSQLHOST", "localhost"),
         port=int(os.environ.get("MYSQLPORT", 3306)),
-        user=os.environ.get("MYSQLUSER"),
-        password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE"),
+        user=os.environ.get("MYSQLUSER", "root"),
+        password=os.environ.get("MYSQLPASSWORD", ""),
+        database=os.environ.get("MYSQLDATABASE", "toko_rama_collection")
     )
